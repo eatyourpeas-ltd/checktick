e5030e285717b944815ea3ef296dde09ff9fa9d6
eatyourpeas <simon@eatyourpeas.co.uk>
2026-03-22 12:11:30 +0000

    removes django admin login but leave access for superusers, bump patch

--- checktick_app/admin.py
+++ checktick_app/admin.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.admin import AdminSite
 from django.contrib.admin.apps import AdminConfig
+from django.http import Http404
 
 
 class CheckTickAdminSite(AdminSite):
@@ -13,6 +14,11 @@ class CheckTickAdminSite(AdminSite):
         return bool(
             request.user and request.user.is_active and request.user.is_superuser
         )
+
+    def login(self, request, extra_context=None):  # type: ignore[override]
+        # Do not expose the Django admin login form. Superusers must already be
+        # authenticated via the main application login before accessing /admin/.
+        raise Http404
 
 
 class CheckTickAdminConfig(AdminConfig):
